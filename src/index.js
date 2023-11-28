10563aae03a896438bd618be1a614bbf65b539d2
František Vojáček <fr.vojacek@email.cz>
2023-11-28 13:39:26 +0100

    fix: correct string to integer conversion

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const path = require('path');
-const testPath = path.join(__dirname, '..', 'tests', 'test5.in');
+const testPath = path.join(__dirname, '..', 'tests', 'test1.in');
 const input = fs.readFileSync(testPath).toString();
 
-let rows = input.split(/\r?\n/)
+let rows = input.split(/\r?\n/).filter(n => n)
 let size = rows[0].split(' ')
-let sizeX = Number(size[0]), sizeY = Number(size[1])
-let amount = Number(rows[sizeY + 1])
+let sizeX = parseInt(size[0]), sizeY = parseInt(size[1])
+let amount = parseInt(rows[sizeY + 1])
 
-let map = rows.slice(1, sizeY + 1).map((row) => row.split('').map(Number))
+let map = rows.slice(1, sizeY + 1).map((row) => row.split('').slice(0, sizeX).map(Number))
 let additionInstructions = rows.slice(sizeY + 2, sizeY + amount + 2).map((row) => row.split(' ').map(Number))
 additionInstructions.sort((a, b) => a[0] - b[0])
 
@@ -40,16 +40,16 @@ function subtractDust() {
 function addDust() {
   if (additionInstructions.length == 0 || additionInstructions[0][0] != iteration) return
   let currentInstruction = additionInstructions.shift()
-  let x = Number(currentInstruction[1])
-  let y = Number(currentInstruction[2])
-  let count = Number(currentInstruction[3])
+  if (iteration == 0) return
+  let x = parseInt(currentInstruction[1])
+  let y = parseInt(currentInstruction[2])
+  let count = parseInt(currentInstruction[3])
 
   for (let a = y - count + 1; a < y + count; a++) {
     if (map[a] === undefined) continue
     for (let b = x - count + 1; b < x + count; b++) {
       if (map[a][b] === undefined) continue
       let newValue = Math.abs(y - a) > Math.abs(x - b) ? count - Math.abs(y - a) : count - Math.abs(x - b)
-      //map[a][b] = newValue > 9 ? 9 : newValue
       if(map[a][b] + newValue > 9)
         map[a][b] = 9
       else
